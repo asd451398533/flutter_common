a49c053bf106c88eb860de5b9e1f771d68599320
林生雨 <linshengyu@igengmei.com>
2020-06-11 19:34:43 +0800

    commit

--- lib/commonModel/toast/NativeToast.dart
+++ lib/commonModel/toast/NativeToast.dart
@@ -7,23 +7,24 @@ import 'package:fluttertoast/fluttertoast.dart';
 
 class NativeToast {
   static int lastTime;
+  static String lastWorld;
 
   static void showNativeToast(String text) {
     showNativeToastWithTime(text, false);
-  }
-
-  static void showNativeToastNotFast(String text) {
-    if (lastTime == null ||
-        DateTime.now().millisecondsSinceEpoch - lastTime > 2000) {
-      showNativeToastWithTime(text, false);
-    }
-    lastTime = DateTime.now().millisecondsSinceEpoch;
   }
 
   static void showNativeToastWithTime(String text, bool long) {
     if (text == null) {
       text = "null";
     }
+    if (lastTime != null &&
+        lastWorld != null &&
+        DateTime.now().millisecondsSinceEpoch - lastTime < 1000 &&
+        lastWorld == text) {
+      return;
+    }
+    lastTime = DateTime.now().millisecondsSinceEpoch;
+    lastWorld = text;
     Fluttertoast.showToast(
         msg: text,
         toastLength: long ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT,
